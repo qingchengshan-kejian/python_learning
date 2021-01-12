955c8aacc0268cdea5a40ba1f5694a1515d3f95c
zhangyue <yueyue_freedom@163.com>
2021-01-12 23:22:29 +0800

    add files#20210112

---
+++ python_cookbook/2_6_Searching_and_Replacing_Case.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+
+import re
+
+text ='UPPER PYTHON, lower python, Mixed Python'
+print(re.findall('python', text, flags=re.IGNORECASE))
+
+print(re.sub('python', 'snake', text, flags=re.IGNORECASE))
+
